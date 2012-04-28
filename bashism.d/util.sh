ca2afdefc7976071494f6e3826e22475a5698a96
Trevor Joynson (trevorj) <github@skywww.net>
2012-04-28 08:09:24 -0400

    Fold util and auto add to cmd_path

--- bashism.d/util.sh
+++ bashism.d/util.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## -> b.split($by, $str)
+## {{{ -> b.split($by, $str)
 ## If $by is empty, split by $IFS into array $ret
 function bashism.util.split() {
 	ret=()
@@ -8,7 +8,11 @@ function bashism.util.split() {
 		ret[${#ret[@]}]="$i"
 	done
 }
+## }}}
 
-## -> b.util.is_function($name)
+## {{{ -> b.util.is_function($name)
 function bashism.util.is_function() { declare -f -F "$1" >/dev/null 2>&1; }
+## }}}
+
+bashism.cmd_path.push util
 
