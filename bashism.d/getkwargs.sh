9529d4ddacaedd995b8ab211e9e7228c138e6acb
Trevor Joynson (trevorj) <github@skywww.net>
2013-05-06 07:35:44 -0400

    Update

--- bashism.d/getkwargs.sh
+++ bashism.d/getkwargs.sh
@@ -18,12 +18,14 @@ function bashism.getkwargs.getkwargs() {
 
     local k= v=
     for v in "$@"; do
-        if [[ "$v" =~ $kw_re ]]; then
-            kwargs[${BASH_REMATCH[1]}]="${BASH_REMATCH[2]}"
-        ## For people with Bash from 1999
+        ## For people with Bash from 1999, it doesn't do very strict keyword
+        ## checking, however.
         #if [[ "$v" = *=* ]]; then
         #    local k="${v#*=}" v="${v%%=*}"
         #    kwargs[$k]="$v"
+
+        if [[ "$v" =~ $kw_re ]]; then
+            kwargs[${BASH_REMATCH[1]}]="${BASH_REMATCH[2]}"
         else
             args[${#args[@]}]="$v"
         fi
@@ -31,30 +33,11 @@ function bashism.getkwargs.getkwargs() {
 
     ## Bash will munge the newlines during expansion if it's being expanded,
     ## so we need to make sure that there are no newlines, with a seperator.
-    #echo "$(declare -p args); $(declare -p kwargs)"
     local ret=
     ret+="$(declare -p args)"
     ret+=";"
     ret+="$(declare -p kwargs)"
     echo "$ret"
-}
-
-
-function bashism.getkwargs.test.getkwargs() {
-    if [[ -n "$*" ]]; then
-        e "--- Testing with \"$@\":"
-        # Declare automatically makes variables local if you're in a function,
-        # so don't be worried about your global scope getting mucked here ;)
-        eval "$(bashism.getkwargs.getkwargs "$@")"
-        declare -p args kwargs
-        return
-    fi
-
-    # TODO Make this actually verify that the output == input, ie a test ;)
-    bashism.getkwargs.test.getkwargs omg=yes=yes2 arg0 arg1 munch=faces=whatsits
-    bashism.getkwargs.test.getkwargs "omg=yes=yes2 arg0" "arg1 munch=faces=whatsits"
-    bashism.getkwargs.test.getkwargs "omg=yes=yes2 arg0" "arg1 munch=faces=whatsits"
-    bashism.getkwargs.test.getkwargs "omg=yes=yes2 arg0" "arg1 munch=faces=whatsits"
 }
 
 
